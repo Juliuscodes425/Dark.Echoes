8025044d11f17da458a54f6dfc108e2c5dbcfc0e
Dylan Jules <dylan.jules@oakwood.edu>
2025-06-05 16:15:55 -0400

    This is all I can do based on the guided and Demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,50 @@
+import { episodeList } from "./data";
+import { useState } from "react";
 export default function App() {
-  // TODO
+  const episode = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!episode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to watch.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.id}</h2>
+        <p>{selectedEpisode.title}</p>
+        {selectedEpisode.title && <p>{selectedEpisode.description} </p>}
+      </section>
+    );
+  }
+
+  /** List of episodes that a user can select from */
+  function watchList() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ul className="episodeInfo">
+          {episodeList.map((episodes) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+  return (
+    <>
+      <header>
+        <h1>The Dark Echoes</h1>
+      </header>
+      <h3>choose your episode</h3>
+      <watchList />
+      <EpisodeDetails />
+    </>
+  );
 }
